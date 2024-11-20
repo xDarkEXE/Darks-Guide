32342a447f68411eb9677d809e468e9b54d178cf
DarkEXE <aconnor0213+cryptic@gmail.com>
2024-11-20 11:33:14 +0000

    Update +page.svelte
    fixing up the home page due to spelling mistakes and sentences not making sense

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     <div class="max-xl:w-1/2 flex flex-col gap-[30px] p-5 xl:p-10">
         <h1 class="text-4xl">Welcome to my website!</h1>
         <p class="text-2xl">
-            This website is be all about console commands for a lot of games.
+            This website will be about console commands for a lot of different games.
             So what are console commands?
             <br>
             "Console commands are instructions to the game that players can use to cheat, experiment, or work around bugs."
